6ad28c9d5cc7afccc2d885bf1083dc6d5561d79f
adeki <s.ikeda.1102@gmail.com>
2021-08-21 11:57:10 +0900

    up

--- sql/dockerinit.sh
+++ sql/dockerinit.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-cat ./sql/for_doker.sql ./sql/0_Schema.sql | docker-compose exec -T db mysql --defaults-file=/dev/null -u root isucondition
+cat ./sql/for_doker.sql ./sql/0_Schema.sql ./sql/1_InitData.sql | docker-compose exec -T db mysql --defaults-file=/dev/null -u root isucondition
