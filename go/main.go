3dd728fae6015ee6519102e9d278c40ac69332b8
adeki <s.ikeda.1102@gmail.com>
2021-08-21 11:40:05 +0900

    update

--- go/main.go
+++ go/main.go
@@ -180,7 +180,7 @@ func getEnv(key string, defaultValue string) string {
 
 func NewMySQLConnectionEnv() *MySQLConnectionEnv {
 	return &MySQLConnectionEnv{
-		Host:     getEnv("MYSQL_HOST", "127.0.0.1"),
+		Host:     getEnv("MYSQL_HOST", "db"),
 		Port:     getEnv("MYSQL_PORT", "3306"),
 		User:     getEnv("MYSQL_USER", "isucon"),
 		DBName:   getEnv("MYSQL_DBNAME", "isucondition"),
