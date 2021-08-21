a520a8791268d169c9980fa1582f716a930144e0
adeki <s.ikeda.1102@gmail.com>
2021-08-21 14:04:48 +0900

    webdav image

--- go/main.go
+++ go/main.go
@@ -24,6 +24,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	"github.com/studio-b12/gowebdav"
 )
 
 const (
@@ -50,6 +51,10 @@ var (
 	jiaJWTSigningKey *ecdsa.PublicKey
 
 	postIsuConditionTargetBaseURL string // JIAへのactivate時に登録する，ISUがconditionを送る先のURL
+
+	webdavURL      = "https://isucondition.t.isucon.dev/dav/"
+	webdavUser     = ""
+	webdavPassword = ""
 )
 
 type Config struct {
@@ -569,6 +574,14 @@ func postIsu(c echo.Context) error {
 		}
 	}
 
+	imagePath := strings.Join([]string{jiaIsuUUID, jiaUserID, "image"}, "/")
+
+	client := gowebdav.NewClient(webdavURL, webdavUser, webdavPassword)
+
+	if err := client.Write(imagePath, image, 0644); err != nil {
+		return err
+	}
+
 	tx, err := db.Beginx()
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)
@@ -700,17 +713,10 @@ func getIsuIcon(c echo.Context) error {
 
 	jiaIsuUUID := c.Param("jia_isu_uuid")
 
-	var image []byte
-	err = db.Get(&image, "SELECT `image` FROM `isu` WHERE `jia_user_id` = ? AND `jia_isu_uuid` = ?",
-		jiaUserID, jiaIsuUUID)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return c.String(http.StatusNotFound, "not found: isu")
-		}
+	imagePath := strings.Join([]string{jiaIsuUUID, jiaUserID, "image"}, "/")
 
-		c.Logger().Errorf("db error: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
+	client := gowebdav.NewClient(webdavURL, webdavUser, webdavPassword)
+	image, _ := client.Read(imagePath)
 
 	return c.Blob(http.StatusOK, "", image)
 }
