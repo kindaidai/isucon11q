82f14cde0fc1e3d29fab87ee2676f770a587638f
Kazumasa Okanuma <kazumasa.okanuma@gmail.com>
2021-08-21 13:28:56 +0900

    add index.

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -15,6 +15,9 @@ CREATE TABLE `isu` (
    PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
+ALTER TABLE `isu` ADD INDEX character_index(`character`);
+ALTER TABLE `isu` ADD INDEX jia_user_id_index(`jia_user_id`);
+
 CREATE TABLE `isu_condition` (
   `id` bigint AUTO_INCREMENT,
   `jia_isu_uuid` CHAR(36) NOT NULL,
@@ -25,6 +28,8 @@ CREATE TABLE `isu_condition` (
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+ALTER TABLE `isu_condition` ADD INDEX jia_isu_uuid_index(`jia_isu_uuid`);
 
 CREATE TABLE `user` (
   `jia_user_id` VARCHAR(255) PRIMARY KEY,
